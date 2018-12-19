972ae8fb4a1ba7c3c4a0d4631413fc9edabb1d70
hardel99 <harold-delgado1999@hotmail.com>
2018-12-18 21:23:11 -0600

    Main Settings and Overture finished
    Ya esta un esqueleto básico, ahora solo queda empezar el embedamiento con la clase que sera el proyecto de unity exportado!

--- app/src/main/java/com/interfaz/hardel/targetsar/MainActivity.java
+++ app/src/main/java/com/interfaz/hardel/targetsar/MainActivity.java
@@ -1,13 +1,27 @@
 package com.interfaz.hardel.targetsar;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button start;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.iniciemos);
+        start.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        /*Intent unity = new Intent(this, unityClassName.class);
+        startActivity(unity);*/
     }
 }
